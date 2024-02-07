548469aba7c99fddd6019f9e94bf500aa60bb416
nefertitistits <158286363+nefertitistits@users.noreply.github.com>
2024-02-07 15:25:15 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c, sum;
+    printf("First value");
+    scanf("%d", & a);
+    printf("second value");
+    scanf("%d", & b);
+    printf("third value");
+    scanf("%d", & c);
+    sum= a+b+c;
+    printf("total=%d",sum);
+    return 0;
+}
+
