a7f8d0adf3598ef0ec69227f05dbbc4ba08c931d
mohan maina <158286363+nefertitistits@users.noreply.github.com>
2024-02-21 00:30:53 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-int main()
-{
-    int a, b, c, sum;
-    printf("First value");
-    scanf("%d", & a);
-    printf("second value");
-    scanf("%d", & b);
-    printf("third value");
-    scanf("%d", & c);
-    sum= a+b+c;
-    printf("total=%d",sum);
+int main() {
+    float pounds, kilograms;
+    int start, end, increment;
+
+    // Accepting input from the user
+    printf("Enter the starting value (in pounds): ");
+    scanf("%d", &start);
+    printf("Enter the ending value (in pounds): ");
+    scanf("%d", &end);
+    printf("Enter the increment value: ");
+    scanf("%d", &increment);
+
+    // Adding heading line
+    printf("\nPounds\tKilograms\n");
+    printf("-----------------------\n");
+
+    for (pounds = start; pounds <= end; pounds += increment) {
+        kilograms = pounds * 0.453592; // 1 pound is approximately 0.453592 kilograms
+        printf("%6.2f\t%6.2f\n", pounds, kilograms);
+    }
+
     return 0;
 }
 
