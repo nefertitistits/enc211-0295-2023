f5ad6a18d7647f625c120f1f957176a48c9219ca
mohan maina <158286363+nefertitistits@users.noreply.github.com>
2024-02-21 00:32:07 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 int main() {
-    float pounds, kilograms;
+    float fahrenheit, celsius;
     int start, end, increment;
 
     // Accepting input from the user
-    printf("Enter the starting value (in pounds): ");
+    printf("Enter the starting value (in Fahrenheit): ");
     scanf("%d", &start);
-    printf("Enter the ending value (in pounds): ");
+    printf("Enter the ending value (in Fahrenheit): ");
     scanf("%d", &end);
     printf("Enter the increment value: ");
     scanf("%d", &increment);
 
     // Adding heading line
-    printf("\nPounds\tKilograms\n");
+    printf("\nFahrenheit\tCelsius\n");
     printf("-----------------------\n");
 
-    for (pounds = start; pounds <= end; pounds += increment) {
-        kilograms = pounds * 0.453592; // 1 pound is approximately 0.453592 kilograms
-        printf("%6.2f\t%6.2f\n", pounds, kilograms);
+    for (fahrenheit = start; fahrenheit <= end; fahrenheit += increment) {
+        celsius = (fahrenheit - 32) * 5 / 9;
+        printf("%8.2f\t%6.2f\n", fahrenheit, celsius);
     }
 
     return 0;
